88c739030f429bc99ddd10e7dcbe6dc03628e773
Autobot007 <71343269+Autobot007@users.noreply.github.com>
2022-07-29 19:07:34 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 ﻿
-// See https://aka.ms/new-console-template for more information
+namespace Day3
+{
+    public class DataType
+    {
+        string name = "Sarfaraaz";
+        int no = 1;
+        double grno = 54321;
+        float avg = 23.4567f;
+        char grade = 'A';
+        long phone = 9876543210;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("declaring variable");
+           
+        }
+    }
 
-Console.WriteLine("Hello, World!");
-string name = "Sarfaraaz";
-int no = 1;
-double grno = 54321;
-float avg = 23.4567f;
-char grade = 'A';
-long phone = 9876543210;
 
+}
