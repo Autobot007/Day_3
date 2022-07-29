c33f994af6bf663547c51d4c3421b27b21eb4514
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-07-29 18:41:17 +0530

    [Sarfaraaz] - Data Type added

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+string name = "Sarfaraaz";
+int no = 1;
+double grno = 54321;
+float avg = 23.4567f;
+char grade = 'A';
+long phone = 9876543210;
+
